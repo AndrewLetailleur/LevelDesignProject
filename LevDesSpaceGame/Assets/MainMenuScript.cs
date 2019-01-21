f4e4222be451a3ea5763fab432852ca8ab84911b
Andrew Letailleur <31889767+AndrewLetailleur@users.noreply.github.com>
2019-01-21 09:26:41 +0000

    set up
    Getting stuff set up, so I don't forget to do so later. Hassle wise. Project programming wise... At least, until I get rushed "insisted" on doing programming by seat of pants, without plans in hand...

---
+++ LevDesSpaceGame/Assets/MainMenuScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;//for the Info switchin;
+
+public class MainMenuScript : MonoBehaviour {
+    
+    //play the first level, after menu. Scene wise
+    public void PlayGame() {SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);}
+
+    public void LoadGame() { }//to load 'saved' save state, scene/save database management wise
+    
+    public void QuitGame() {
+        Debug.Log("Game has closed");
+        Application.Quit();
+    }
+
+    /* No longer needed code, as GameObject.SetActive on OnClick() Unity does the trick just fine
+    public void Credits() {}
+    public void Info() {}
+    public void Story() {}
+    public void OptionsMenu() {}
+    public void GoBack() {}
+    */ /// end of no longer needed code
+}
