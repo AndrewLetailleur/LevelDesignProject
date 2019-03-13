f81f6d1331c8761ee9f27d1bc1965e1038a31079
Andrew Letailleur <31889767+AndrewLetailleur@users.noreply.github.com>
2019-03-13 07:46:26 +0000

    archiving changes

--- LevDesSpaceGame/Assets/Scripts/ObjectWraparound.cs
+++ LevDesSpaceGame/Assets/Scripts/ObjectWraparound.cs
@@ -5,14 +5,51 @@ using UnityEngine;
 public class ObjectWraparound : MonoBehaviour
 {
     private GameObject player;//player REF
-    public float MaxDist;
+
+    public bool RotateFlag = false;     //determines if object should rotate
+    public bool RandomTrig = false;
+
+
 
     //reference values
-    public float curX_Dist, curZ_Dist;          //distance by an X/Z axis
-    private float selfX, selfZ, playX, playZ;   //self = this.object, play = player object
+    public float MaxDist;               //final version should be dependant on camera aspect bounds
     
+    //wraparound variables
+    public float curX_Dist, curZ_Dist;  //distance by an X/Z axis
+    private float selfX, selfZ;         //self = this.object, 
+    private float playX, playZ;         //play = player object
+
+    //rotate velocity variables
+    public float Velo_X, Velo_Z;        //determines velocity
+    public float Min_RNG, Max_RNG;      //random values for velocity calculating
+
+
     // Start is called before the first frame update
-    void Start() { player = GameObject.FindGameObjectWithTag("Player"); }
+    void Start() {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        ///
+
+        if (RotateFlag) /*if yes, cue rotation code*/
+        {
+            if (RandomTrig)
+            {
+                Velo_X = Random.Range(Min_RNG, Max_RNG);
+                Velo_Z = Random.Range(Min_RNG, Max_RNG);
+            }
+            Rigidbody rb = GetComponent<Rigidbody>();
+
+            if (this.gameObject.GetComponent<Rigidbody>() != null)
+            {
+                rb.velocity = new Vector3(Velo_X, 0, Velo_Z);
+            }
+            else
+            {
+                Debug.Log("WARNING, no Rigidbody detected!");
+            }
+        }//end of rotate code
+    }
     
     public void GetDistance()
     {
